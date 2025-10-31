84e9dfcdcf59d9da3acc3006792bb24891bb77e7
rubbal123 <rubbalblockcoders@gmail.com>
2025-10-31 19:06:18 +0530

    cors

--- apex_backend/src/app.ts
+++ apex_backend/src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import helmet from 'helmet';
+import express from "express";
+// import morgan from 'morgan';
+import cors from "cors";
+// import helmet from 'helmet';
 // import userRoutes from './routes/user.route';
-import authRoutes from './routes/auth.route';
-import { errorHandler } from './middlewares/error.middleware'
+import authRoutes from "./routes/auth.route";
+import { errorHandler } from "./middlewares/error.middleware";
 // import { authenticateToken } from './middlewares/auth.middleware';
 import okxRoutes from "./routes/okx.routes";
 import safeheronRoutes from "./routes/safeheron.routes";
@@ -14,11 +14,20 @@ import "./crons/balanceCron.cron";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: [
+      "*", // allow all
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
 // app.use('/api/users', authenticateToken, userRoutes);
 app.use("/api/v1/okx", okxRoutes);
 app.use("/api/v1/safeheron", safeheronRoutes);
